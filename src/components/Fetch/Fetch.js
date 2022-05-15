f4a34ef8c2339dd1e3db61bbb1436f49f2f78687
Dilshan Liyanage <dilshan.l@synergenhealth.com>
2022-05-15 15:05:48 +0530

    remove usestate

--- src/components/Fetch/Fetch.js
+++ src/components/Fetch/Fetch.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 
 function Fetch() {
-
-  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
 
