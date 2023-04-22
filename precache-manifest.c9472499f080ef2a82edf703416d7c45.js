759b9cf79b900752334f45d660c2aca0269f6d2c
Dilshan Liyanage <nelly.dil@hotmail.com>
2023-04-22 22:47:15 +0800

    Updates

---
+++ precache-manifest.c9472499f080ef2a82edf703416d7c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "555341966f94ce1aa2a6",
+    "url": "/react-tic-tac-toe/static/css/main.378c9a20.chunk.css"
+  },
+  {
+    "revision": "555341966f94ce1aa2a6",
+    "url": "/react-tic-tac-toe/static/js/main.55534196.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-tic-tac-toe/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "b7299bd6c641dbc5b842",
+    "url": "/react-tic-tac-toe/static/js/2.b7299bd6.chunk.js"
+  },
+  {
+    "revision": "384c36e5391f83fed71e",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.384c36e5.js"
+  },
+  {
+    "revision": "bad3e2662d983a6c5b3c7ac37a3fd5cd",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
